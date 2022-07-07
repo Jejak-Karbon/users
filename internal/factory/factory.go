99d1931177042823c72fc2ed2533345d95810434
Syarifuddin <syarifuddinahmads.me@gmail.com>
2022-07-07 23:26:55 +0700

    update repository config

--- internal/factory/factory.go
+++ internal/factory/factory.go
@@ -7,11 +7,13 @@ import (
 
 type Factory struct {
 	ProductRepository repository.Product
+	UserRepository    repository.User
 }
 
 func NewFactory() *Factory {
 	db := database.GetConnection()
 	return &Factory{
 		ProductRepository: repository.NewProduct(db),
+		UserRepository:    repository.NewUser(db),
 	}
 }
